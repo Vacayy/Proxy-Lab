3b0b72631728cd33426f7f240076f6e5e4b50a2b
JuyeongKim201 <juyeong.kim.201@gmail.com>
2023-11-22 22:55:17 +0900

    #3. tiny 서버 POST 요청 테스트

--- tiny/cgi-bin/adder.c
+++ tiny/cgi-bin/adder.c
@@ -19,12 +19,15 @@ int main(void)
         n1 = atoi(arg1);
         n2 = atoi(arg2);
     }
+    // printf("buf: --->>>> %s\n", buf);
 
-    sprintf(content, "QUERY_STRING=%s", buf); // HTML 형식으로 생성
+    sprintf(content, "QUERY_STRING=%s", buf); // HTML 형식으로 생성    
+    
     sprintf(content, "Welcome to add.com: ");
     sprintf(content, "%sTHE Internet addition portal.\r\n<p>", content);
     sprintf(content, "%sThe answer is: %d + %d = %d\r\n<p>", content, n1, n2, n1 + n2);
     sprintf(content, "%sThanks for visiting!\r\n", content);
+    
 
     printf("Connection: close\r\n");
     printf("Content-length: %d\r\n", (int)strlen(content));
--- tiny/tiny.c
+++ tiny/tiny.c
@@ -12,11 +12,11 @@
 void doit(int fd);
 void read_requesthdrs(rio_t *rp);
 int parse_uri(char *uri, char *filename, char *cgiargs);
-// void serve_static(int fd, char *filename, int filesize, char *method);
 void serve_static(int fd, char *filename, int filesize);
 void get_filetype(char *filename, char *filetype);
 void serve_dynamic(int fd, char *filename, char *cgiargs, char *method);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
+void serve_login(int fd, char *id, char *pw);
 
 int main(int argc, char **argv)
 {
@@ -62,12 +62,31 @@ void doit(int fd)
     printf("Request headers:\n");
     printf("%s", buf);
     sscanf(buf, "%s %s %s", method, uri, version); // buf 문자열에서 method, uri, version을 읽어온다.
+  
+    read_requesthdrs(&rio); // 요청 헤더를 읽고 파싱한다.(요청 헤더를 읽어서 클라이언트가 요청한 추가 정보를 처리한다.)
+    // POST 요청 처리
+    if (strcasecmp(method, "POST") == 0) 
+    {
+        char content[MAXLINE];
+        Rio_readlineb(&rio, content, MAXLINE);
+        char *id = strstr(content, "id=");
+        char *pw = strstr(content, "pw=");
+        
+        if (id && pw) {
+            id += 3; // "id=" 다음부터 ID 값
+            pw += 3; // "pw=" 다음부터 PW 값
+            // ID와 PW를 serve_login 함수로 전달
+            serve_login(fd, id, pw);
+        } else {
+            clienterror(fd, method, "400", "Bad Request", "Invalid POST data");
+        }
+        return;
+    } // GET 요청 처리
     if (strcasecmp(method, "GET"))
     { // 요청 메소드가 GET이 아니면 에러 처리
         clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
         return;
     }
-    // read_requesthdrs(&rio); // 요청 헤더를 읽고 파싱한다.(요청 헤더를 읽어서 클라이언트가 요청한 추가 정보를 처리한다.)
 
     /* URI 파싱 */
     is_static = parse_uri(uri, filename, cgiargs); // 요청이 정적 콘텐츠인지 동적 콘텐츠인지 파악한다.
@@ -147,6 +166,8 @@ int parse_uri(char *uri, char *filename, char *cgiargs)
         strcat(filename, uri);             // filename에 uri를 이어 붙인다.
         if (uri[strlen(uri) - 1] == '/')   // uri가 /로 끝나면
             strcat(filename, "home.html"); // home.html 파일을 filename에 추가
+        else if (uri[strlen(uri) - 1] == 'n')
+            printf("!!!!!!!!!!!!!\n\n\n");
         return 1;                          // 정적 컨텐츠일 때는 1 리턴
     }
     else
@@ -172,7 +193,7 @@ void serve_static(int fd, char *filename, int filesize)
 
     /* 응답 헤더 전송 */
     get_filetype(filename, filetype); // 파일 타입 결정
-    sprintf(buf, "HTTP/1.0 200 OK\r\n"); // 상태 코드
+    sprintf(buf, "HTTP/1.0 200 OK\r\n"); // 상태 코드 <- 헤더에 필수임 + 상태코드 괜히 있는 거 아님 제대로 써야됨
     sprintf(buf, "%sServer: Tiny Web Server\r\n", buf); // 서버 이름
     sprintf(buf, "%sConnection: close\r\n", buf); // 연결 방식
     sprintf(buf, "%sContent-length: %d\r\n", buf, filesize); // 컨텐츠 길이
@@ -213,12 +234,26 @@ void serve_dynamic(int fd, char *filename, char *cgiargs, char *method)
     sprintf(buf, "Server: Tiny Web Server\r\n");
     Rio_writen(fd, buf, strlen(buf));
 
-    if (Fork() == 0) // 자식 프로세스 포크
+    /* 부모 프로세스(웹 서버)는 자식 프로세스를 생성하고, 자식 프로세스가 CGI 프로그램을 실행하면서 요청을 처리하고 응답을 생성 */
+    if (Fork() == 0) 
     {
         setenv("QUERY_STRING", cgiargs, 1);   // QUERY_STRING 환경 변수를 URI에서 추출한 CGI 인수로 설정
         // setenv("REQUEST_METHOD", method, 1);  
-        Dup2(fd, STDOUT_FILENO);              // 자식 프로세스의 표준 출력을 클라이언트 소켓에 연결된 파일 디스크립터로 변경
+        Dup2(fd, STDOUT_FILENO);              // 자식 프로세스의 표준 출력을 클라이언트 소켓에 연결된 파일 디스크립터로 변경. 이렇게 함으로써 자식 프로세스에서 출력되는 모든 데이터는 클라이언트로 전송됨
+        // 이는 CGI 프로그램이 생성한 데이터(예: 웹 페이지 내용)를 클라이언트로 보내기 위해서
         Execve(filename, emptylist, environ); // Run CGI Program 
     }
     Wait(NULL);
+}
+
+void serve_login(int fd, char *id, char *pw) {
+    // 이 부분에서 ID와 PW에 대한 간단한 처리 수행
+
+    // ID와 PW를 터미널에 출력
+    printf("<<<<<[로그인 요청 받음] ID: %s, PW: %s>>>>>\n", id, pw);
+
+    // 클라이언트에게 응답 보내기
+    char response[MAXLINE];
+    snprintf(response, sizeof(response), "HTTP/1.1 200 OK\r\nContent-type: text/plain\r\n\r\nLogin successful\n");
+    Rio_writen(fd, response, strlen(response));
 }
